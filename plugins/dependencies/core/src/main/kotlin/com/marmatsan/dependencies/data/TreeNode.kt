c0fabf859cd8e53195d7e04f6d30de304b859576
mmateos <mmateos@hiberus.com>
2023-09-06 13:47:07 +0200

    feat: Implementing MultiModuleApp

--- plugins/dependencies/core/src/main/kotlin/com/marmatsan/dependencies/data/TreeNode.kt
+++ plugins/dependencies/core/src/main/kotlin/com/marmatsan/dependencies/data/TreeNode.kt
@@ -1,28 +1,37 @@
-package com.marmatsan.dependencies
+package com.marmatsan.dependencies.data
 
-data class TreeNode<T>(
-    val value: T
+data class TreeNode(
+    val nodeData: NodeData
 ) {
-    private val children: MutableList<TreeNode<T>> by lazy {
+    private val children: MutableList<TreeNode> by lazy {
         mutableListOf()
     }
 
-    fun add(child: TreeNode<T>) = children.add(child)
-    fun add(vararg children: TreeNode<T>) = this.children.addAll(children)
+    fun add(vararg children: TreeNode) = this.children.addAll(children)
 
     private fun hasChildren(): Boolean = children.size >= 1
     private fun isLeaf(): Boolean = !hasChildren()
 
     companion object {
         fun findLeafNodePaths(
-            node: TreeNode<NodeData>,
+            node: TreeNode,
             path: MutableList<String> = mutableListOf(),
             result: MutableList<String> = mutableListOf()
         ): MutableList<String> {
+            val nodeData = node.nodeData
+            path.add(nodeData.dependencyId)
 
-            path.add(node.value.group)
             if (node.isLeaf()) {
-                result.add("${path.joinToString(separator = "") { it }}:${node.value.version}")
+                val joinedPath = path.joinToString(separator = "") { it }
+                val version = nodeData.version
+
+                nodeData.artifacts?.forEach { artifact ->
+                    // Library
+                    result.add("$joinedPath:${artifact}:$version")
+                } ?:
+                // Plugin
+                result.add("$joinedPath:$version")
+
             } else {
                 path.add(".")
                 for (child in node.children) {
