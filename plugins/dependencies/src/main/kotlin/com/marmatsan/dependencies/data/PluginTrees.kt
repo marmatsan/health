d1e611e427a70c0af2a40564dcdbf691a0bbeab6
mmateos <mmateos@hiberus.com>
2023-09-19 11:41:10 +0200

    feat: Improved dependencies tree structures

---
+++ plugins/dependencies/src/main/kotlin/com/marmatsan/dependencies/data/PluginTrees.kt
@@ -0,0 +1,31 @@
+package com.marmatsan.dependencies.data
+
+import com.marmatsan.dependencies.data.NodeData.Plugin as Plugin
+
+val comPluginTree = tree(Plugin(id = "com")) {
+    tree(Plugin(id = "android")) {
+        tree(Plugin(id = "application", version = "8.1.1"))
+        tree(Plugin(id = "library", version = "8.1.1"))
+    }
+    tree(Plugin(id = "google")) {
+        tree(Plugin(id = "devtools")) {
+            tree(Plugin(id = "ksp", version = "1.9.0-1.0.13"))
+        }
+        tree(Plugin(id = "dagger")) {
+            tree(Plugin(id = "hilt")) {
+                tree(Plugin(id = "android", version = "2.48"))
+            }
+        }
+    }
+}
+
+
+val orgPluginTree = tree(Plugin(id = "org")) {
+    tree(Plugin(id = "jetbrains")) {
+        tree(Plugin(id = "kotlin")) {
+            tree(Plugin(id = "android", version = "1.9.0"))
+        }
+    }
+}
+
+val pluginTrees = listOf(comPluginTree, orgPluginTree)
--- plugins/dependencies/src/main/kotlin/com/marmatsan/dependencies/plugin/DependenciesPlugin.kt
+++ plugins/dependencies/src/main/kotlin/com/marmatsan/dependencies/plugin/DependenciesPlugin.kt
@@ -17,71 +17,69 @@ class DependenciesPlugin : Plugin<Settings> {
 private fun DependencyResolutionManagement.buildVersionCatalogs() {
     versionCatalogs {
         create("libs") {
-            val androidXLibraries = TreeNode.getDependencies(androidXLibraryTree())
-            createLibraries(androidXLibraries)
-            val comLibraries = TreeNode.getDependencies(comLibraryTree())
-            createLibraries(comLibraries)
+            libraryTrees.forEach {
+                createLibraries(it.getDependencies())
+            }
         }
         create("libsCompose") {
-            val androidXComposeLibraryTree = TreeNode.getDependencies(androidXComposeLibraryTree())
-            createLibraries(androidXComposeLibraryTree)
+            createLibraries(androidXComposeLibraryTree.getDependencies())
         }
         create("plugins") {
-            val comPlugins = TreeNode.getDependencies(comPluginTree())
-            createPlugins(comPlugins)
-            val orgPlugins = TreeNode.getDependencies(orgPluginTree())
-            createPlugins(orgPlugins)
+            pluginTrees.forEach {
+                createPlugins(it.getDependencies())
+            }
         }
     }
 }
 
 private fun VersionCatalogBuilder.createLibraries(dependencies: List<Dependency>) {
-    dependencies.forEach { library -> // TODO: Library has to be always of type Dependency.Library, but it's of type Dependency
-        if (library is Dependency.Library) {
-            val libraryGroup = library.group
-            val artifactsGroups = library.artifactsGroups
+    dependencies.forEach { dependency ->
 
-            if (artifactsGroups?.size == 1 && artifactsGroups.first().artifacts.size == 1) {
-                val libraryArtifact = artifactsGroups.first().artifacts.first()
-                val libraryVersion = artifactsGroups.first().version
-                library(
-                    libraryGroup,
-                    "$libraryGroup:$libraryArtifact:$libraryVersion"
+        val library = dependency as Dependency.Library
+
+        val libraryGroup = library.group
+        val artifactsGroups = library.artifactsGroups
+
+        if (artifactsGroups?.size == 1 && artifactsGroups.first().artifacts.size == 1) {
+            val libraryArtifact = artifactsGroups.first().artifacts.first()
+            val libraryVersion = artifactsGroups.first().version
+            library(
+                libraryGroup,
+                "$libraryGroup:$libraryArtifact:$libraryVersion"
+            )
+        } else {
+            artifactsGroups?.forEach { artifactsGroup ->
+                val bundleName = "$libraryGroup.${artifactsGroup.name}"
+                version(
+                    bundleName,
+                    artifactsGroup.version
                 )
-            } else {
-                artifactsGroups?.forEach { artifactsGroup ->
-                    val bundleName = "$libraryGroup.${artifactsGroup.name}"
-                    version(
-                        bundleName,
-                        artifactsGroup.version
-                    )
-                    val artifactsAliases = mutableListOf<String>()
-                    artifactsGroup.artifacts.forEach { artifact ->
-                        val artifactAlias = "$libraryGroup.$artifact"
-                        library(
-                            artifactAlias,
-                            libraryGroup,
-                            artifact
-                        ).versionRef(bundleName)
-                        artifactsAliases.add(artifactAlias)
-                    }
-                    bundle(
-                        bundleName,
-                        artifactsAliases
-                    )
+                val artifactsAliases = mutableListOf<String>()
+                artifactsGroup.artifacts.forEach { artifact ->
+                    val artifactAlias = "$libraryGroup.$artifact"
+                    library(
+                        artifactAlias,
+                        libraryGroup,
+                        artifact
+                    ).versionRef(bundleName)
+                    artifactsAliases.add(artifactAlias)
                 }
+                bundle(
+                    bundleName,
+                    artifactsAliases
+                )
             }
         }
     }
 }
 
 private fun VersionCatalogBuilder.createPlugins(plugins: List<Dependency>) {
-    plugins.forEach { plugin -> // TODO: Plugin has to be always of type Dependency.Plugin, but it's of type Dependency
-        if (plugin is Dependency.Plugin && plugin.version != null) { // TODO: Plugin version is always not null here
+    plugins.forEach { dependency ->
+        val plugin = dependency as Dependency.Plugin
+        if (plugin.version != null) // TODO: Plugin version is always not null here
             plugin(
                 plugin.id,
                 plugin.id
             ).version(plugin.version)
-        }
     }
 }
